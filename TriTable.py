7179a32d222546406e9cae07025fc3a88a20fce8
Jaco <62554433+JC-Joubert@users.noreply.github.com>
2020-05-08 01:16:18 +0200

    Update TriTable.py

--- TriTable.py
+++ TriTable.py
@@ -1,6 +1,6 @@
 # A program that uses nested for loops to create a number pyramid.
 
 for i in range(1 , 10):
-    for j in range(i + 1):  # (comment for myself) for j in range(1, i + 1): this piece of code will help you remove the 0 
+    for j in range(i + 1):
         print (i * j, end = " ")
     print(" ")
